9217225e712098bdae3ea79927cbf0d672de47c1
MatheusBarcellosDev <matheusbarcellos61@gmail.com>
2022-06-21 16:09:41 -0300

    iniciando o projeto

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const routes = require("./routes/routes");
+const cors = require("cors");
+
+const app = express();
+const port = 5000;
+
+app.use(express.json());
+app.use(cors());
+app.use(routes);
+
+app.listen(process.env.PORT || port, console.log(`Rodando na porta ${port}`));
